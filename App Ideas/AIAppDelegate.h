56aadae18edf4a0c6e72db6907d6e3639f9a8c96
Joshua Howland <development@jkhowland.com>
2014-05-26 23:23:25 -0600

    Initial Commit

---
+++ App Ideas/AIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AIAppDelegate.h
+//  App Ideas
+//
+//  Created by Joshua Howland on 5/26/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App Ideas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  App Ideas
+//
+//  Created by Joshua Howland on 5/26/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AIAppDelegate class]));
+    }
+}
